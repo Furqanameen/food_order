ee1770c669cb6dc8a5d79bea67952f685867dafc
Furqanbinameen <furqanbinameen@gmail.com>
2019-04-19 16:33:46 +0500

    pdf

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -32,11 +32,13 @@ ActiveAdmin.register User do
   permit_params :email, :password, :publisher_id,
   tags_attributes: [:id, :email, :_destroy]
   index download_links: [:pdf]
+
  index download_links: proc{ current_user.can_view_download_links? }
   controller do
+
    # if you want /admin/pages/12345.pdf
     def index
-      binding.pry
+      # binding.pry
       @user = User.all
       super do |format|
         format.pdf do
@@ -63,6 +65,26 @@ ActiveAdmin.register Team do
 
 end
 ActiveAdmin.register Member do
+  # config.sort_order = 'name_asc'
+  config.filters = true
+  filter :last_name
+  filter :first_name
+  filter :created_at
+  # config.sort_order = 'last_name_asc'
+  # config.sort_order = 'name_desc'
+  index download_links: [:pdf,:xml,:csv,:json]
+  controller do
+    def index
+      @user= Member.all
+      super do |format|
+        format.pdf do
+          render :pdf=>"",
+          template: "admin/order_items/show.html.erb"
+        end
+      end
+
+    end
+  end
   permit_params :Email, :location,:first_name,:last_name,
   tags_attributes: [:id,:Email,:location,:first_name,:last_name, :_destroy]
 end
